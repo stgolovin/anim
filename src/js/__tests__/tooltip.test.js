826ffcb693e9dcfdac9fa64988d58682edd0aa37
Stanislav Golovin <golovin.ds93@gmail.com>
2024-07-21 23:00:58 +0300

    initial commit

---
+++ src/js/__tests__/tooltip.test.js
@@ -0,0 +1,26 @@
+import { Tooltip } from '../tooltip';
+
+test('showTooltip adds tooltip to DOM', () => {
+  document.body.innerHTML = '<button type="button" class="btn">Click to toggle popover</button>';
+  const button = document.querySelector('.btn');
+  const tooltipFactory = new Tooltip();
+
+  tooltipFactory.showTooltip('Title', 'Text', button);
+
+  const tooltip = document.querySelector('.tooltip');
+  expect(tooltip).not.toBeNull(); // Проверяем, что элемент с классом 'tooltip' присутствует в DOM
+  expect(tooltip.querySelector('.tooltip-title').textContent).toBe('Title');
+  expect(tooltip.querySelector('.tooltip-message').textContent).toBe('Text');
+});
+
+test('removeTooltip removes tooltip from DOM', () => {
+  document.body.innerHTML = '<button type="button" class="btn">Click to toggle popover</button>';
+  const button = document.querySelector('.btn');
+  const tooltipFactory = new Tooltip();
+
+  const tooltipId = tooltipFactory.showTooltip('Title', 'Text', button);
+  tooltipFactory.removeTooltip(tooltipId);
+
+  const tooltip = document.querySelector('.tooltip');
+  expect(tooltip).toBeNull(); // Проверяем, что элемент с классом 'tooltip' отсутствует в DOM
+});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+};
